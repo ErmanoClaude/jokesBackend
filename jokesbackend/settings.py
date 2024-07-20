a6a30d53cbc59865ef1081209fce2f174f74b6b6
Ermano Claude <ermano.claude@cix.csi.cuny.edu>
2024-07-20 04:08:36 -0400

    added aws cors

--- jokesbackend/settings.py
+++ jokesbackend/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://jokesfrontend.s3-website.us-east-2.amazonaws.com/"]
 
 ROOT_URLCONF = 'jokesbackend.urls'
 
